ead04622a4f2e884e95064d4bcb73cb2452ed1c8
Yu Mitsuhori <ymbullseye@gmail.com>
2020-05-02 20:00:02 +0900

    initial_commit

---
+++ android/app/src/main/kotlin/you/mee/state_notifier_test_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package you.mee.state_notifier_test_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
